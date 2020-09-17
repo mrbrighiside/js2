0ad305fa71335d20cde7fe40dc05f06c3ee81a75
AndrewGetman <mrbrighiside@gmail.com>
2020-09-17 13:21:16 +0700

    firstCommit

--- project/js/main.js
+++ project/js/main.js
@@ -1,31 +1,78 @@
-'use strict';
+class ProductList {
+  #goods;
 
-const products = [
-    {id: 1, title: 'Notebook', price: 20000},
-    {id: 2, title: 'Mouse', price: 1500},
-    {id: 3, title: 'Keyboard', price: 5000},
-    {id: 4, title: 'Gamepad', price: 4500},
-];
+  constructor(container = '.products') {
+    this.container = container;
+    this.#goods = [];
+    this.allProducts = [];
+    this.fullCartPrice = 0
 
-const renderProduct = ({title}, {price}, img = 'img/exampleImg.jpg' ) => {
-    return `<div class="product-item">
-                <h3>${title}</h3>
-                <img src="${img}" alt="">
-                <p>${price}</p>
-                <button class="by-btn">Добавить в корзину</button>
-              </div>`;
-};
+    this.#fetchGoods();
+    this.#render();
+    this.summaryAllCart()
+  }
 
-const renderProducts = (list) => {
-    const productList = list.map((product) => {
-        return renderProduct(product, product);
-    }).join(' ');
-    console.log(productList);
-    document.querySelector('.products').innerHTML = productList;
-};
+  #fetchGoods() {
+    this.#goods = [
+      {id: 1, title: 'Notebook', price: 20000},
+      {id: 2, title: 'Mouse', price: 1500},
+      {id: 3, title: 'Keyboard', price: 5000},
+      {id: 4, title: 'Gamepad', price: 4500},
+    ];
+  }
 
-renderProducts(products);
-// TASK 3. When map convert array to string automatically join with comma,
-// here I used join() with space separator
+  #render() {
+    const block = document.querySelector(this.container);
 
-// TASK 2. sorry, I think I didn't understand second Task ;*(
+    for (let product of this.#goods) {
+      const productObject = new ProductItem(product);
+
+      this.allProducts.push(productObject);
+
+      block.insertAdjacentHTML('beforeend', productObject.getHTMLString());
+    }
+  }
+
+  summaryAllCart() {
+    for (let product of this.allProducts) {
+      this.fullCartPrice += product.price
+    }
+    console.log(this.fullCartPrice)
+  }
+}
+
+class ProductItem {
+  constructor(product, img = 'https://placehold.it/200x150') {
+    this.title = product.title;
+    this.price = product.price;
+    this.id = product.id;
+    this.img = img;
+  }
+  getHTMLString() {
+    return `<div class="product-item" data-id="${this.id}">
+              <img src="${this.img}" alt="Some img">
+              <div class="desc">
+                  <h3>${this.title}</h3>
+                  <p>${this.price} \u20bd</p>
+                  <button class="buy-btn">Купить</button>
+              </div>
+          </div>`;
+  }
+}
+
+const list = new ProductList();
+
+class Cart {
+  #goods
+  constructor(container) {
+  }
+  renderCart(){}
+  summaryAllProducts(){}
+}
+
+class cartItem {
+  #goods
+  constructor(product) {
+  }
+  pushProductsToCart(){}
+}
